cf2302784574417b6d30302d6f5c25eb90f8df8b
aaravraghani <aaravraghani@gmail.com>
2025-11-27 16:25:23 +0700

    Create page.tsx

---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,22 @@
+import { SignIn } from '@clerk/nextjs'
+
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-background-gray px-4">
+      <div className="w-full max-w-md">
+        <SignIn 
+          appearance={{
+            elements: {
+              rootBox: 'mx-auto',
+              card: 'shadow-xl',
+            },
+          }}
+          routing="path"
+          path="/sign-in"
+        />
+      </div>
+    </div>
+  )
+}
+
+
